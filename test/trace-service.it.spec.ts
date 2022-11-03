50ffed94172df6761b1a73cf7430861d2f6c840d
jojczykp <pawel.jojczyk@gmail.com>
2022-11-03 16:54:40 +0000

    Add integration test covering https://github.com/expressjs/express/issues/4396

--- test/trace-service.it.spec.ts
+++ test/trace-service.it.spec.ts
@@ -1,19 +1,40 @@
-import express from 'express'
+import express, { json } from 'express'
 import request from 'supertest'
-import { getTraceContext, traceMiddleware } from '../src/trace-service'
+import {getTraceContext, traceMiddleware} from '../src/trace-service'
 
 describe('Trace Service deliver express middleware that', () => {
-    it('should inject trace context (even if wrapped with async middleware)', (done) => {
+    it('should inject trace context', (done) => {
         let server = express()
             .use(traceMiddleware)
-            .get('/', (req, res) => {
-                res.json(getTraceContext()).end()
-            })
+            .get('/', (req, res) => res.json(getTraceContext()).end())
             .listen()
 
         request(server)
             .get('/')
             .set('traceparent', '00-123456789abcd-12ab-01')
+            .expect({ version: '00', traceId: '123456789abcd', parentId: '1234567890abcdef', isSampled: true })
+            .end((err) => {
+                try {
+                    server.close()
+                    expect(err).toBeNull()
+                } finally {
+                    done()
+                }
+            })
+    })
+
+    it('should inject trace context even if wrapped with async middleware', (done) => {
+        let server = express()
+            .use(traceMiddleware)
+            .use(json()) // middleware with async call if body present (read from socket)
+            .post('/', (req, res) => res.json(getTraceContext()).end())
+            .listen()
+
+        request(server)
+            .post('/')
+            .set('traceparent', '00-123456789abcd-12ab-01')
+            .set('content-type', 'application/json')
+            .send({some: 'body'})
             .expect({ version: '00', traceId: '123456789abcd', parentId: '1234567890abcdef', isSampled: true })
             .end((err) => {
                 try {
